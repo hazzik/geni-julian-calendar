675a498a42d622a7938f3db8cb7e7e84b819780b
Alex Zaytsev <hazzik@gmail.com>
2024-04-29 10:58:15 +1000

    chore: replace web-ext-submit with web-ext

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const package = require('./package.json');
 
-module.exports = {
+module.exports = (env) => ({
 	devtool: 'source-map',
 	mode: 'production',	
 	plugins: [
@@ -12,10 +12,16 @@ module.exports = {
 					transform(content) {
 						const manifest = JSON.parse(content.toString());
 						manifest.version = package.version;
+						if (env.AMO_EXTENSION_ID) {
+							manifest.browser_specific_settings = {
+								...manifest.browser_specific_settings,
+								gecko: { id: env.AMO_EXTENSION_ID }
+							};
+						}
 						return JSON.stringify(manifest);
 					}
 				}
     	    ]
 		})
 	]
-};
+});
